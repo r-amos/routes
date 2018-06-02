2fa87499a411b02ba11d1286f3549fa29bd9780e
Robert Amos <rob@robamos.co.uk>
2018-06-02 21:39:07 +0100

    Configuring Webpack For React & Webpack Dev Server

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,10 +1,33 @@
 const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 const configuration = {
+  mode: "production",
   entry: "./app/src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, "src")],
+        loader: "babel-loader"
+      },
+      {
+        test: /\.html/,
+        include: [path.resolve(__dirname, "src")],
+        loader: "html-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./app/src/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
 
 module.exports = configuration;
